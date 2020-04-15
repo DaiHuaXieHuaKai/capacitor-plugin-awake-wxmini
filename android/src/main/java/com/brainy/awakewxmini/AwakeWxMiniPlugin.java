7907d03eebc680900dba19e4a2d364261b57777b
赵颖 <oncwnuMi12RSmI6gR0NkQhnhdrbo@git.weixin.qq.com>
2020-04-16 00:18:26 +0800

    修改回调

--- android/src/main/java/com/brainy/awakewxmini/AwakeWxMiniPlugin.java
+++ android/src/main/java/com/brainy/awakewxmini/AwakeWxMiniPlugin.java
@@ -1,12 +1,11 @@
 package com.brainy.awakewxmini;
 
-import com.getcapacitor.JSObject;
+import android.content.Context;
+
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.tencent.mm.opensdk.constants.ConstantsAPI;
-import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
@@ -17,8 +16,12 @@ public class AwakeWxMiniPlugin extends Plugin {
     public static final int MINIPROGRAM_TYPE_TEST = 1;//开发版
     public static final int MINIPROGRAM_TYPE_PREVIEW = 2;//体验版
 
+    private static IWXAPI api;
+    private static PluginCall currentCall;
+
     @PluginMethod()
     public void awake(PluginCall call) {
+        currentCall = call;
         String appId = call.getString("appId", "");//应用id
         String miniId = call.getString("miniId", "");//小程序id
         String path = call.getString("path", "");//打开小程序页面地址
@@ -31,7 +34,10 @@ public class AwakeWxMiniPlugin extends Plugin {
             call.error("miniId is required");
             return;
         }
-        IWXAPI api = WXAPIFactory.createWXAPI(getContext(), appId);
+        if(api == null){
+            api = WXAPIFactory.createWXAPI(getContext(), appId);
+            api.registerApp(appId);
+        }
         WXLaunchMiniProgram.Req req = new WXLaunchMiniProgram.Req();
         req.userName = miniId;
         req.path = path;
@@ -39,13 +45,12 @@ public class AwakeWxMiniPlugin extends Plugin {
         api.sendReq(req);
     }
 
-    public void onResp(BaseResp resp) {
-        if (resp.getType() == ConstantsAPI.COMMAND_LAUNCH_WX_MINIPROGRAM) {
-            WXLaunchMiniProgram.Resp launchMiniProResp = (WXLaunchMiniProgram.Resp) resp;
-            String extraData =launchMiniProResp.extMsg;
-            JSObject res = new JSObject();
-            res.put("data",extraData);
-            notifyListeners("launchResponse",res);
-        }
+    public static IWXAPI getWxApi(Context ctx){
+        return api;
     }
+
+    public static PluginCall getCall(){
+        return currentCall;
+    }
+
 }
